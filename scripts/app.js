f33d7301ceac2f477a30fec5e5460d82b51cf754
faisalchoudhry <faisalchoudhry05@gmail.com>
2021-11-22 23:35:43 +0500

    changes been done.

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,29 @@
 var app = angular.module('app', []);
 app.controller('searchCtrl', function ($scope, $http) {
+    /*Globl Variables*/
     $scope.back = true;
     $scope.searchResultState = true;
     $scope.searchBoxState = false;
-    $scope.app_id = "0ab49580-c84f-44d4-875f-d83760ea2cfe";
+    $scope.app_id = "0ab49580-c84f-44d4-875f-d83760ea2cfe"; /*API KEY*/
+
+    /*Get Artist Name from API*/
     $scope.getArtistNameFromApi = function (artist_name) {
-        $http.get("https://rest.bandsintown.com/artists/" + artist_name + "?app_id=" + $scope.app_id + "")
-            .then(function (response) {
-                $scope.artist_result = response.data;
-                $scope.searchResultState = false;
-                $scope.searchBoxState = false;
-                // console.log(response.data);
-            });
+        if ($scope.validateSearch(artist_name)) {
+            $http.get("https://rest.bandsintown.com/artists/" + artist_name + "?app_id=" + $scope.app_id + "")
+                .then(function (response) {
+                    $scope.artist_result = response.data;
+                    if (response.data.error === 'Not Found') {
+                        $scope.artist_result = [];
+                        $scope.searchResultState = true;
+                    } else {
+                        $scope.searchResultState = false;
+                        $scope.searchBoxState = false;
+                        window.localStorage.setItem(artist_name, JSON.stringify($scope.artist_result));
+                    }
+                });
+        }
     }
+    /*Get Artist Events from API*/
     $scope.getArtistEventsFromApi = function (artist_name) {
         $http.get("https://rest.bandsintown.com/artists/" + artist_name + "/events?app_id=" + $scope.app_id + "")
             .then(function (response) {
@@ -20,9 +31,12 @@ app.controller('searchCtrl', function ($scope, $http) {
                 $scope.back = false;
                 $scope.searchResultState = true;
                 $scope.searchBoxState = true;
+                window.localStorage.setItem(artist_name, JSON.stringify($scope.artist_events));
                 // console.log(response.data);
             });
     }
+
+    /*Back to result*/
     $scope.backToResult = function () {
         // $(".event_detail_wrapper").html('');
         $scope.artist_events = [];
@@ -31,13 +45,15 @@ app.controller('searchCtrl', function ($scope, $http) {
         $scope.searchBoxState = false;
     }
 
-    // $scope.enter_listner = addEventListener("keyup", function(event) {
-    //     // Number 13 is the "Enter" key on the keyboard
-    //     if (event.keyCode === 13) {
-    //         // Cancel the default action, if needed
-    //         event.preventDefault();
-    //         // Trigger the button element with a click
-    //         document.getElementById("myBtn").click();
-    //     }
-    // });
+    /*validate search box method*/
+    $scope.validateSearch = function (artist_name) {
+        var x = artist_name;
+        if (x === "" || typeof (x) === "undefined") {
+            alert("Artist name must be filled out");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 });
